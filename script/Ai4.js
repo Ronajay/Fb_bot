6cfe8a80da8a4fc28ecf0c90980763e713f29aaf
Ronajay <florrona3@gmail.com>
2024-07-13 22:21:31 +0800

    Update Ai4.js

--- script/Ai4.js
+++ script/Ai4.js
@@ -2,7 +2,7 @@ const { get } = require('axios');
 
 module.exports.config = {
   name: 'ai4',
-  credits: "con",
+  credits: "Anonymous",
   version: '1.0.0',
   role: 0,
   aliases: ["Gpt"],
@@ -17,9 +17,9 @@ module.exports.run = async function ({ api, event, args }) {
     api.sendMessage(msg, event.threadID, event.messageID);
   }
 
-  const url = "https://hercai.onrender.com/v3/hercai";
+  const url = "Have a good day";
 
-  if (!question) return sendMessage("Please provide a question to begin ✨");
+  if (!question) return sendMessage("Please provide a question");
 
   try {
     const response = await get(`${url}?question=${encodeURIComponent(question)}`);
